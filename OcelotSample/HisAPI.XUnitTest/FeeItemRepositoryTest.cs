c9df6721291199bac7cf80fae1c2b1955d539007
桂素伟 <axzxs2001@163.com>
2017-12-05 13:48:13 +0800

    FeeItem单元测试。

--- OcelotSample/HisAPI.XUnitTest/FeeItemRepositoryTest.cs
+++ OcelotSample/HisAPI.XUnitTest/FeeItemRepositoryTest.cs
@@ -3,9 +3,14 @@ using System;
 using Xunit;
 using DapperPlus;
 using HisAPI.Model.Repository;
+using System.Collections.Generic;
 
 namespace HisAPI.XUnitTest
 {
+    /// <summary>
+    /// FeeItemRepository仓储测试
+    /// </summary>
+    [Trait("FeeItem仓储层", "FeeItemRepository")]
     public class FeeItemRepositoryTest
     {
         /// <summary>
@@ -20,6 +25,30 @@ namespace HisAPI.XUnitTest
         {
             _dbMock = new Mock<IDapperPlusDB>();
             _feeItemRepository = new FeeItemRepository(_dbMock.Object);
+        }
+
+        /// <summary>
+        /// 按名称模糊查询收费项目测试
+        /// </summary>
+        [Fact]
+        public void GetFeeItem_NullName_Return()
+        {
+            var list = new List<dynamic>() { new {name="name1" },new { name="name2"} };
+            _dbMock.Setup(db => db.Query<dynamic>(It.IsAny<string>(), null, null, false, null, null)).Returns(list);
+            var feeItems = _feeItemRepository.GetFeeItem("");
+            Assert.Equal(2, feeItems.Count);
+        }
+
+        /// <summary>
+        /// 按名称模糊查询收费项目测试
+        /// </summary>
+        [Fact]
+        public void GetFeeItem_Default_Return()
+        {
+            var list = new List<dynamic>() { new { name = "name1" }, new { name = "name2" } };
+            _dbMock.Setup(db => db.Query<dynamic>(It.IsAny<string>(),  It.IsAny<object>(), null,false, null, null)).Returns(list);
+            var feeItems = _feeItemRepository.GetFeeItem("amxl");
+            Assert.Equal(2, feeItems.Count);
         }
     }
 }
