37cb9e09ea4ab3699d5979205d6c481bfbc2ad9a
桂素伟 <axzxs2001@163.com>
2017-12-31 14:13:03 +0800

    添加报警发邮件功能.

--- OcelotSample/HisAPI/Controllers/HisUserController.cs
+++ OcelotSample/HisAPI/Controllers/HisUserController.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using HisAPI.Model.Repository;
 using Common;
+using MimeKit;
+using MailKit.Net.Smtp;
+using System.Text;
 
 namespace HisAPI.Controllers
 {
@@ -61,13 +64,63 @@ namespace HisAPI.Controllers
             });
         }
        // [AllowAnonymous]
-        [HttpPost("/abc")]
-        public IActionResult ABC()
+        [HttpPost("/notice")]
+        public IActionResult Notice()
         {
             var bytes = new byte[10240];
             var i = Request.Body.ReadAsync(bytes, 0, bytes.Length);
-           var content= System.Text.Encoding.UTF8.GetString(bytes).Trim('\0');
+            var content= System.Text.Encoding.UTF8.GetString(bytes).Trim('\0');
+            SendEmail(content);
             return Ok();
+        }
+
+        void SendEmail(string content)
+        {
+           dynamic list= Newtonsoft.Json.JsonConvert.DeserializeObject(content);
+            if(list != null&&list.Count>0)
+            {
+                var emailBody = new StringBuilder("健康检查故障:\r\n");            
+                foreach(var noticy in list)
+                {
+                    emailBody.AppendLine($"--------------------------------------");
+                    emailBody.AppendLine($"Node:{noticy.Node}");
+                    emailBody.AppendLine($"Service ID:{noticy.ServiceID}");
+                    emailBody.AppendLine($"Service Name:{noticy.ServiceName}");
+                    emailBody.AppendLine($"Check ID:{noticy.CheckID}");
+                    emailBody.AppendLine($"Check Name:{noticy.Name}");
+                    emailBody.AppendLine($"Check Status:{noticy.Status}");
+                    emailBody.AppendLine($"Check Output:{noticy.Output}");
+                    emailBody.AppendLine($"--------------------------------------");
+                }
+                var message = new MimeMessage();
+                message.From.Add(new MailboxAddress("gswmicroservice", "gswmicroservice@163.com"));
+                message.To.Add(new MailboxAddress("285130205", "285130205@qq.com"));
+
+                message.Subject = "作业报警";
+                message.Body = new TextPart("plain") { Text = emailBody.ToString() };
+                using (var client = new SmtpClient())
+                {
+
+                    client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+                    client.Connect("smtp.163.com", 25, false);
+                    client.AuthenticationMechanisms.Remove("XOAUTH2");
+                    client.Authenticate("gswmicroservice", "gsw790622");
+                    client.Send(message);
+                    client.Disconnect(true);
+                }
+            }
+       
+        }
+
+        /// <summary>
+        /// 健康检查
+        /// </summary>
+        /// <returns></returns>
+        [AllowAnonymous]
+        [HttpGet("/health")]
+        public IActionResult Health()
+        {
+            return Content("健康检查：正常");
         }
     }
 
