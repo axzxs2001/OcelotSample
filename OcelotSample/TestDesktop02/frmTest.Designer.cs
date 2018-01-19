e6407b7acbd5d61568d0c28ef70d1622d303850d
桂素伟 <axzxs2001@163.com>
2018-01-19 13:52:05 +0800

    refit请求封装完成。

---
+++ OcelotSample/TestDesktop02/frmTest.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace TestDesktop02
+{
+    partial class frmTest
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.butLogin = new System.Windows.Forms.Button();
+            this.btnQuery = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txbQuery = new System.Windows.Forms.TextBox();
+            this.txbPassword = new System.Windows.Forms.TextBox();
+            this.txbUserName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 59);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(866, 480);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // butLogin
+            // 
+            this.butLogin.Location = new System.Drawing.Point(346, 16);
+            this.butLogin.Name = "butLogin";
+            this.butLogin.Size = new System.Drawing.Size(75, 23);
+            this.butLogin.TabIndex = 0;
+            this.butLogin.Text = "登录";
+            this.butLogin.UseVisualStyleBackColor = true;
+            this.butLogin.Click += new System.EventHandler(this.butLogin_Click);
+            // 
+            // btnQuery
+            // 
+            this.btnQuery.Location = new System.Drawing.Point(756, 15);
+            this.btnQuery.Name = "btnQuery";
+            this.btnQuery.Size = new System.Drawing.Size(75, 23);
+            this.btnQuery.TabIndex = 1;
+            this.btnQuery.Text = "查询";
+            this.btnQuery.UseVisualStyleBackColor = true;
+            this.btnQuery.Click += new System.EventHandler(this.btnQuery_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txbQuery);
+            this.panel1.Controls.Add(this.txbPassword);
+            this.panel1.Controls.Add(this.txbUserName);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.butLogin);
+            this.panel1.Controls.Add(this.btnQuery);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(866, 59);
+            this.panel1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(471, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "搜索：";
+            // 
+            // txbQuery
+            // 
+            this.txbQuery.Location = new System.Drawing.Point(518, 17);
+            this.txbQuery.Name = "txbQuery";
+            this.txbQuery.Size = new System.Drawing.Size(219, 21);
+            this.txbQuery.TabIndex = 6;
+            this.txbQuery.Text = "amxl";
+            // 
+            // txbPassword
+            // 
+            this.txbPassword.Location = new System.Drawing.Point(231, 17);
+            this.txbPassword.Name = "txbPassword";
+            this.txbPassword.PasswordChar = '*';
+            this.txbPassword.Size = new System.Drawing.Size(100, 21);
+            this.txbPassword.TabIndex = 4;
+            this.txbPassword.Text = "222222";
+            // 
+            // txbUserName
+            // 
+            this.txbUserName.Location = new System.Drawing.Point(63, 17);
+            this.txbUserName.Name = "txbUserName";
+            this.txbUserName.Size = new System.Drawing.Size(100, 21);
+            this.txbUserName.TabIndex = 2;
+            this.txbUserName.Text = "ggg";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(192, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "密码：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "用户名：";
+            // 
+            // frmTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(866, 539);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmTest";
+            this.Text = "frmTest";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button butLogin;
+        private System.Windows.Forms.Button btnQuery;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txbQuery;
+        private System.Windows.Forms.TextBox txbPassword;
+        private System.Windows.Forms.TextBox txbUserName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ OcelotSample/TestDesktop02/frmTest.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Refit;
+
+namespace TestDesktop02
+{
+    public partial class frmTest : Form
+    {
+        public frmTest()
+        {
+            InitializeComponent();
+        }
+
+        ITestWebApi _testAPI;
+        Result _token;
+        private void butLogin_Click(object sender, EventArgs e)
+        {
+            //登录获取Token
+            var testAPI = RestService.For<ITestWebApi>("http://127.0.0.1:5000");
+            _token = testAPI.Login(txbUserName.Text, txbPassword.Text).GetAwaiter().GetResult();
+
+            //初始化通用接口
+            _testAPI = RestService.For<ITestWebApi>(new HttpClient(new AuthenticatedHttpClientHandler(_token.access_token)) { BaseAddress = new Uri("http://127.0.0.1:5000") });
+            butLogin.Enabled = false;
+            
+        }
+
+        private void btnQuery_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = _testAPI.Query(txbQuery.Text).GetAwaiter().GetResult();
+        }
+    }
+
+
+    public interface ITestWebApi
+    {
+        [Post("/authapi/login")]
+        Task<Result> Login(string username, string password);
+
+
+        [Get("/hisapi/getfeeitems")]
+        [Headers("Authorization: Bearer")]
+        Task<dynamic> Query(string name);
+    }
+    class AuthenticatedHttpClientHandler : HttpClientHandler
+    {   
+
+        string _token;
+        public AuthenticatedHttpClientHandler(string token)
+        {
+            _token = token;
+        }
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            // See if the request has an authorize header
+            var auth = request.Headers.Authorization;
+            if (auth != null)
+            {
+                // var token = await getToken().ConfigureAwait(false);
+                request.Headers.Authorization = new AuthenticationHeaderValue(auth.Scheme, _token);
+            }
+
+            return await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
+        }
+    }
+
+    #region 辅助类
+    public class Result
+    {
+        public bool status { get; set; }
+        public string access_token { get; set; }
+
+        public string expires_in { get; set; }
+        public string token_type { get; set; }
+    }
+
+    public class User
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+    }
+    #endregion
+}
